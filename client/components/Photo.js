ec8c5891396620fefaebd92214da332a0f7d0e76
Keyu Lin <mlchaellin222@gmail.com>
2018-02-14 19:37:38 -0500

    finished photogrid

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,4 +1,4 @@
-import React from 'React';
+import React from 'react';
 import { Link } from 'react-router';
 
 import CSSTransitionGroup from 'react-addons-css-transition-group';
@@ -14,23 +14,18 @@ const Photo =React.createClass({
               className='grid-photo'/>
           </Link>
 
-          <CSSTransitionGroup transitionName='like'
-            transitionEnterTimeout={500}
-            transitionLeaveTimeout={500}>
-            <span key={post.likes} className="like-heart">
-            {post.likes}</span>
-          </CSSTransitionGroup>
+          
           <figcaption>
             <p>{post.caption}</p>
             <div className="control-buttons">
-              <button className="likes">&hearts; {post.likes}
+              <button onClick={this.props.increment.bind(null,i)} className="likes">&hearts; {post.likes}
               </button>
               <Link className="button" to={'/view/${post.code}'}>
                 <span className="comment-count">
                   <span className="speech-bubble">
                   </span>
                   {comments[post.code]? comments[post.code].length :0}
-                  
+
                 </span>
               </Link>
             </div>
